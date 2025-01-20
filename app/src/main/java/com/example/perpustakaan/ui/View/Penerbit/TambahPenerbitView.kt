813907aaa15719900756e0f3a480a99fde8b49aa
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 01:21:15 +0700

    menambahkan kode TambahPenerbitView

--- app/src/main/java/com/example/perpustakaan/ui/View/Penerbit/TambahPenerbitView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/View/Penerbit/TambahPenerbitView.kt
@@ -1,2 +1,178 @@
 package com.example.perpustakaan.ui.View.Penerbit
 
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.perpustakaan.Navigasi.DestinasiNavigasi
+import com.example.perpustakaan.ui.ViewModel.Penerbit.InsertPenerbitUiEvent
+import com.example.perpustakaan.ui.ViewModel.Penerbit.InsertPenerbitUiState
+import com.example.perpustakaan.ui.ViewModel.Penerbit.InsertPenerbitViewModel
+import com.example.perpustakaan.ui.ViewModel.Penulis.InsertPenulisUiEvent
+import com.example.perpustakaan.ui.ViewModel.Penulis.InsertPenulisUiState
+import com.example.perpustakaan.ui.ViewModel.Penulis.InsertPenulisViewModel
+import com.example.perpustakaan.ui.ViewModel.PenyediaViewModel
+import com.example.perpustakaan.ui.Widget.CustomTopAppBar
+import kotlinx.coroutines.launch
+
+
+object DestinasiTambahPenerbit: DestinasiNavigasi {
+    override  val route = "item_penerbit"
+    override  val titleRes = "Entry Penerbit"
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TambahPenerbitScreen(
+    navigateBack:()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPenerbitViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                judul = "Tambah Penerbit",
+                onKategoriClick = {},
+                onPenulisClick = {},
+                onPenerbitClick = {},
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+
+                },
+                isMenuEnabled = true, // Menampilkan ikon menu
+                isKategoriEnabled = true, // Mengaktifkan menu Dosen
+                isPenulisEnabled = false, // Menonaktifkan menu Mata Kuliah
+                isPenerbitEnabled = false // Menonaktifkan menu Mata Kuliah
+            )
+        }
+    ) { innerPadding ->
+        TambahBodyPenerbit(
+            insertPenerbitUiState = viewModel.penerbituiState,
+            onPenerbitValueChange = viewModel::updateInsertPenerbitState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPenerbit()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun TambahBodyPenerbit(
+    insertPenerbitUiState: InsertPenerbitUiState,
+    onPenerbitValueChange: (InsertPenerbitUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputPenerbit(
+            insertPenerbitUiEvent = insertPenerbitUiState.insertPenerbitUiEvent,
+            onValueChange = onPenerbitValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun FormInputPenerbit(
+    insertPenerbitUiEvent: InsertPenerbitUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange:(InsertPenerbitUiEvent)->Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertPenerbitUiEvent.nama_penerbit,
+            onValueChange = {onValueChange(insertPenerbitUiEvent.copy(nama_penerbit = it))},
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPenerbitUiEvent.id_penerbit.toString(),
+            onValueChange = {onValueChange(insertPenerbitUiEvent.copy(id_penerbit = 0))},
+            label = { Text("Id Penerbit") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPenerbitUiEvent.telepon_penerbit,
+            onValueChange = {onValueChange(insertPenerbitUiEvent.copy(telepon_penerbit = it))},
+            label = { Text("Telepon Penerbit") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPenerbitUiEvent.alamat_penerbit,
+            onValueChange = {onValueChange(insertPenerbitUiEvent.copy(alamat_penerbit = it))},
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+
+        if (enabled){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+
+    }
+}
