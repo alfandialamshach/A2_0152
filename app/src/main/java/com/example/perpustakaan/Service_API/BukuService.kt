598c54f5346370bd9905068aa07b6c50f715a6ed
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 02:57:24 +0700

    Memperbaiki kode BukuService

--- app/src/main/java/com/example/perpustakaan/Service_API/BukuService.kt
+++ app/src/main/java/com/example/perpustakaan/Service_API/BukuService.kt
@@ -3,6 +3,7 @@ package com.example.perpustakaan.Service_API
 import com.example.perpustakaan.model.AllBukuResponse
 import com.example.perpustakaan.model.Buku
 import com.example.perpustakaan.model.BukuDetailResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -19,16 +20,16 @@ interface BukuService {
     @GET("buku/")
     suspend fun getAllBuku(): AllBukuResponse
 
-    @GET("{id_buku}")
+    @GET("buku/{id_buku}")
     suspend fun getBukuID(@Path("id_buku")id_buku:Int):BukuDetailResponse
 
-    @POST("store")
+    @POST("buku/tambahbuku")
     suspend fun insertBuku(@Body buku: Buku)
 
-    @PUT("{id_buku}")
+    @PUT("buku/{id_buku}")
     suspend fun updateBuku(@Path("id_buku")id_buku: Int, @Body buku: Buku)
 
-    @DELETE("{id_buku}")
+    @DELETE("buku/{id_buku}")
     suspend fun deleteBuku(@Path("id_buku")id_buku: Int): retrofit2.Response<Void>
 }
 
