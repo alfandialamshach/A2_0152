d5b0effadd2f29ce41fe9b844ad3d65c5a0df74c
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 02:59:18 +0700

    Memperbaiki kode tipe data di data class Buku

--- app/src/main/java/com/example/perpustakaan/model/Buku.kt
+++ app/src/main/java/com/example/perpustakaan/model/Buku.kt
@@ -1,22 +1,24 @@
 package com.example.perpustakaan.model
 
-import kotlinx.serialization.SerialName
+import com.example.perpustakaan.serializer.DateSerializer
+
 import kotlinx.serialization.Serializable
-import java.sql.Date
+
+import java.util.Date // Menggunakan java.util.Date
 
 @Serializable
 data class Buku(
     val id_buku: Int,
     val nama_buku: String,
     val deskripsi_buku: String,
-    val tanggal_terbit: String,
+    @Serializable(with = DateSerializer::class) // Menambahkan serializer untuk tanggal_terbit
+    val tanggal_terbit: Date, // Ganti dengan java.util.Date
     val status_buku: String,
-    val id_kategori: Int?,
-    val id_penulis: Int?,
-    val id_penerbit: Int?,
-
-
+    val id_kategori: Int,
+    val id_penulis: Int,
+    val id_penerbit: Int,
 )
+
 
 @Serializable
 data class AllBukuResponse(
