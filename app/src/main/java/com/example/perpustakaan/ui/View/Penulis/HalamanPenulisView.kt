8c0712ba770ee34ea630ce412d3b78a8e2eff5e8
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 01:13:03 +0700

    Membuat HalamanPenulisView

--- app/src/main/java/com/example/perpustakaan/ui/View/Penulis/HalamanPenulisView.kt
+++ app/src/main/java/com/example/perpustakaan/ui/View/Penulis/HalamanPenulisView.kt
@@ -1,2 +1,281 @@
 package com.example.perpustakaan.ui.View.Penulis
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.perpustakaan.Navigasi.DestinasiNavigasi
+import com.example.perpustakaan.R
+import com.example.perpustakaan.model.Penulis
+import com.example.perpustakaan.ui.ViewModel.Penulis.HomePenulisUiState
+import com.example.perpustakaan.ui.ViewModel.Penulis.HomePenulisViewModel
+import com.example.perpustakaan.ui.ViewModel.PenyediaViewModel
+import com.example.perpustakaan.ui.Widget.CustomTopAppBar
+
+object DestinasiHomePenulis : DestinasiNavigasi {
+   override val route = "home Penulis"
+  override  val titleRes = "Home Penulis"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePenulis(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    onUpdateClick: (Penulis) -> Unit = {}, // Menambahkan parameter untuk update
+    viewModel: HomePenulisViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                judul = "Penulis",
+                onKategoriClick = {},
+                onPenulisClick = {},
+                onPenerbitClick = {},
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+
+                },
+                isMenuEnabled = true, // Menampilkan ikon menu
+                isKategoriEnabled = true, // Mengaktifkan menu Dosen
+                isPenulisEnabled = false, // Menonaktifkan menu Mata Kuliah
+                isPenerbitEnabled = false // Menonaktifkan menu Mata Kuliah
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Penulis")
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            homePenulisUiState = viewModel.penulisUiState,
+            retryAction = { viewModel.getPenulis() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deletePenulis(it.id_penulis)
+                viewModel.getPenulis()
+            },
+            onUpdateClick = onUpdateClick// Menggunakan fungsi update
+
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homePenulisUiState: HomePenulisUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Penulis) -> Unit = {},
+    onUpdateClick: (Penulis) -> Unit = {}, // Menambahkan parameter untuk update
+    onDetailClick: (Int) -> Unit
+) {
+    when (homePenulisUiState) {
+        is HomePenulisUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomePenulisUiState.Success ->
+            if (homePenulisUiState.penulis.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data penulis", style = MaterialTheme.typography.bodyLarge)
+                }
+            } else {
+                BukuList(
+                    penulis = homePenulisUiState.penulis,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_penulis)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    },
+                    onUpdateClick = onUpdateClick // Menggunakan fungsi update
+                )
+            }
+        is HomePenulisUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier.size(150.dp),
+            painter = painterResource(R.drawable.loadingg),
+            contentDescription = stringResource(R.string.loading)
+        )
+        Text(
+            text = stringResource(R.string.loading),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.error),
+            contentDescription = null,
+            modifier = Modifier.size(150.dp)
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun BukuList(
+    penulis: List<Penulis>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Penulis) -> Unit,
+    onDeleteClick: (Penulis) -> Unit = {},
+    onUpdateClick: (Penulis) -> Unit = {}  // Menambahkan parameter untuk update
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(penulis) { penulis ->
+            BukuCard(
+                penulis = penulis,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(penulis) },
+                onDeleteClick = {
+                    onDeleteClick(penulis)
+                },
+                onUpdateClick = {
+                    onUpdateClick(penulis)  // Menambahkan fungsi update
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun BukuCard(
+    penulis: Penulis,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Penulis) -> Unit = {},
+    onUpdateClick: (Penulis) -> Unit = {}  // Menambahkan parameter untuk update
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = penulis.id_penulis.toString(),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Spacer(Modifier.weight(1f))
+
+                // Tombol Hapus
+                IconButton(onClick = { onDeleteClick(penulis) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.error
+                    )
+                }
+
+                // Tombol Update
+                IconButton(onClick = { onUpdateClick(penulis) }) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,  // Menambahkan icon Edit untuk tombol Update
+                        contentDescription = "Update",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+
+            // Nama Penulis
+            Text(
+                text = penulis.nama_penulis,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            // Nama Biografi
+            Text(
+                text = penulis.biografi,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            // Nama Biografi
+            Text(
+                text = penulis.kontak,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
