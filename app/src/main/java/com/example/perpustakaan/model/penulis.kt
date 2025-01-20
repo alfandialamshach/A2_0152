bc320bae8e7a063cff058d4ad3daefd2119ca3bc
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 03:04:06 +0700

    Memperbaiki kode Typo data class Penulis

--- app/src/main/java/com/example/perpustakaan/model/penulis.kt
+++ app/src/main/java/com/example/perpustakaan/model/penulis.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Penulis(
     val id_penulis: Int,
-    val nama_buku: String,
+    val nama_penulis: String,
     val biografi: String,
     val kontak: String,
 
