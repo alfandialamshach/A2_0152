b8704fb87eb45863ddfd909d6fdbfe756d28b72a
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 01:17:07 +0700

    menambahkan kode TambahKategoriViewModel

--- app/src/main/java/com/example/perpustakaan/ui/ViewModel/Kategori/TambahKategoriViewModel.kt
+++ app/src/main/java/com/example/perpustakaan/ui/ViewModel/Kategori/TambahKategoriViewModel.kt
@@ -1,2 +1,55 @@
 package com.example.perpustakaan.ui.ViewModel.Kategori
 
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.perpustakaan.Repository.KategoriRepository
+import com.example.perpustakaan.model.Kategori
+import kotlinx.coroutines.launch
+
+
+class InsertKategoriViewModel(private val kategori: KategoriRepository) : ViewModel() {
+    var kategoriuiState by mutableStateOf(InsertKategoriUiState())
+
+    fun updateInsertKategoriState(insertKategoriUiEvent: InsertKategoriUiEvent) {
+        kategoriuiState = InsertKategoriUiState(insertKategoriUiEvent =insertKategoriUiEvent)
+    }
+
+    suspend fun insertKategori() {
+        viewModelScope.launch {
+            try {
+                kategori.insertKategori(kategoriuiState.insertKategoriUiEvent.toKategori())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertKategoriUiState(
+    val insertKategoriUiEvent: InsertKategoriUiEvent = InsertKategoriUiEvent(id_kategori = 0)
+)
+
+data class InsertKategoriUiEvent(
+    val id_kategori:Int,
+    val nama_kategori:String = "",
+    val deskripsi_kategori:String = "",
+
+
+    )
+fun InsertKategoriUiEvent.toKategori(): Kategori = Kategori(
+    id_kategori = id_kategori,
+    nama_kategori = nama_kategori,
+    deskripsi_kategori = deskripsi_kategori,
+
+)
+
+fun Kategori.toInsertKategoriUiEvent():InsertKategoriUiEvent = InsertKategoriUiEvent(
+    id_kategori =id_kategori,
+    nama_kategori = nama_kategori,
+    deskripsi_kategori = deskripsi_kategori,
+
+)
