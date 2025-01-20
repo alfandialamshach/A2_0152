a704baa849e1cb673c7086fbd82448006752ac02
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-21 03:05:45 +0700

    merubah kode untuk memanggil PerpustakaanApp

--- app/src/main/java/com/example/perpustakaan/MainActivity.kt
+++ app/src/main/java/com/example/perpustakaan/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.perpustakaan.ui.View.Home.HomeScreen
+import com.example.perpustakaan.ui.ViewModel.PerpustakanApp
 import com.example.perpustakaan.ui.theme.PerpustakaanTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             PerpustakaanTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   HomeScreen(
-                       navigateToItemEntry = {},
+                   PerpustakanApp(
+//                       navigateToItemEntry = {},
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
