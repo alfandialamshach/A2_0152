8cacc15c453470f0ee518ed2afda9da51e18c089
Alfandi Alamshach <fandixpc1@gmail.com>
2025-01-25 20:41:00 +0700

    Menambahakan Kode DetailPenerbitViewModel

---
+++ app/src/main/java/com/example/perpustakaan/ui/ViewModel/Penerbit/DetailPenerbitViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.perpustakaan.ui.ViewModel.Penerbit
+
+import com.example.perpustakaan.Repository.PenerbitRepository
+import com.example.perpustakaan.model.Penerbit
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.perpustakaan.ui.View.Kategori.DestinasiDetailKategori
+import kotlinx.coroutines.launch
+
+
+class DetailPenerbitViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pnr: PenerbitRepository
+) : ViewModel() {
+
+    // Retrieve the id_buku from SavedStateHandle
+    val id_penerbit: Int = checkNotNull(savedStateHandle[DestinasiDetailKategori.ID_Kategori])
+
+    var penerbitUiState = mutableStateOf(InsertPenerbitUiEvent(id_penerbit))
+        private set
+
+    init {
+        ambilDetailPenerbit()
+    }
+
+    // Fetch the book data using id_buku
+    fun ambilDetailPenerbit() {
+        viewModelScope.launch {
+            try {
+                val penerbit = pnr.getPenerbitID(id_penerbit = id_penerbit)
+
+                penerbit?.let {
+                    penerbitUiState.value = it.toDetailPenerbitUiEvent() // Update state with the fetched data
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+
+// Extension function to convert Buku to InsertBukuUiEvent
+fun Penerbit.toDetailPenerbitUiEvent(): InsertPenerbitUiEvent {
+    return InsertPenerbitUiEvent(
+        id_penerbit = id_penerbit,
+        nama_penerbit = nama_penerbit,
+        alamat_penerbit = alamat_penerbit,
+        telepon_penerbit = telepon_penerbit
+    )
+}
